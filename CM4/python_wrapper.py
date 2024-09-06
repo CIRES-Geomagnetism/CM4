ffad80a54f987df3f3f6d03ea114b4c77fb97bf2
CollinKadlecek <coka4389@colorado.edu>
2024-09-06 16:24:51 -0600

    Added all files I have related to CM4 wrapper

---
+++ CM4/python_wrapper.py
@@ -0,0 +1,12 @@
+import numpy as np
+import hello_module
+
+hello_module.hello()
+
+import cm4field_module
+
+print(dir(cm4field_module))
+
+import interactive_cm4_module
+
+print(dir(interactive_cm4_module))
